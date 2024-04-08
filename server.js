8a150c3991ccba3b6f16d4865cf291db6e458f65
A. Lincoln Jarwee <122073281+ajarwee-lin@users.noreply.github.com>
2024-04-08 14:43:06 -0600

    supplies route complete and cors fix

--- server.js
+++ server.js
@@ -14,18 +14,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-    );
-    next();
-  });
+// // Add support for CORS
+app.use(cors());
 
 app.use('/', utilities.handleErrors(router));
 
