88526292f7041a08e2d434b2b3600fd0852ca592
sofiagalkina <gal18024@byui.edu>
2024-04-07 20:23:38 -0600

    delete request for destinations working

--- controllers/destinationController.js
+++ controllers/destinationController.js
@@ -130,4 +130,32 @@ const updateDestination = async (req, res) => {
     }
 };
 
-module.exports = { retrieveAllDestinations, retrieveDestinationById, addDestination, updateDestination };
+// Delete an existing destination from the database
+const deleteDestination = async (req, res) => {
+    try {
+        const db = getDb();
+        if (!db) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const destinationId = req.params.id;
+        if (!ObjectId.isValid(destinationId)) {
+            return res.status(400).json({ error: 'Invalid destination ID' });
+        }
+
+        const destinationsCollection = db.collection('Destination');
+        const response = await destinationsCollection.deleteOne({ _id: new ObjectId(destinationId) });
+
+        if (response.deletedCount > 0) {
+            res.status(200).json({ message: 'Destination deleted successfully' });
+        } else {
+            res.status(404).json({ error: 'Destination not found' });
+        }
+    } catch (error) {
+        console.error('Error deleting destination:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+
+module.exports = { retrieveAllDestinations, retrieveDestinationById, addDestination, updateDestination, deleteDestination };
--- routes/destinationRoute.js
+++ routes/destinationRoute.js
@@ -9,6 +9,7 @@ router.get('/', destinationController.retrieveAllDestinations);
 router.get('/:id', destinationController.retrieveDestinationById);
 router.post('/', destinationController.addDestination);
 router.put('/:id', destinationController.updateDestination);
+router.delete('/:id', destinationController.deleteDestination);
 
 
 
