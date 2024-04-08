1354f3b5cff1738870a6222265976a1d386f9f2f
sofiagalkina <gal18024@byui.edu>
2024-04-07 20:20:09 -0600

    PUT request implemented and running for destinations

--- controllers/destinationController.js
+++ controllers/destinationController.js
@@ -52,6 +52,8 @@ async function retrieveDestinationById(req, res) {
     }
 }
 
+
+// POST create a new destination object in the database 
 const addDestination = async (req, res) => {
     try {
         const db = getDb();
@@ -86,6 +88,46 @@ const addDestination = async (req, res) => {
 };
 
 
+// PUT update an aexisting destination's fields based on the destination's ID:
+const updateDestination = async (req, res) => {
+    try {
+        const db = getDb();
+        if (!db) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
 
+        const destinationId = req.params.id;
+        if (!ObjectId.isValid(destinationId)) {
+            return res.status(400).json({ error: 'Invalid destination ID' });
+        }
 
-module.exports = { retrieveAllDestinations, retrieveDestinationById, addDestination };
+        const destinationUpdates = {
+            name: req.body.name,
+            state: req.body.state,
+            location: req.body.location,
+            description: req.body.description,
+            coordinates: req.body.coordinates,
+            establishedDate: req.body.establishedDate,
+            area: req.body.area,
+            activities: req.body.activities,
+            imageUrl: req.body.imageUrl
+        };
+
+        const destinationsCollection = db.collection('Destination');
+        const response = await destinationsCollection.updateOne(
+            { _id: new ObjectId(destinationId) },
+            { $set: destinationUpdates }
+        );
+
+        if (response.modifiedCount > 0) {
+            res.status(200).json({ message: 'Destination updated successfully' });
+        } else {
+            res.status(404).json({ error: 'Destination not found' });
+        }
+    } catch (error) {
+        console.error('Error updating destination:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+module.exports = { retrieveAllDestinations, retrieveDestinationById, addDestination, updateDestination };
--- routes/destinationRoute.js
+++ routes/destinationRoute.js
@@ -7,5 +7,10 @@ router.use(express.json());
 
 router.get('/', destinationController.retrieveAllDestinations);
 router.get('/:id', destinationController.retrieveDestinationById);
-router.post('/', destinationController.addDestination )
+router.post('/', destinationController.addDestination);
+router.put('/:id', destinationController.updateDestination);
+
+
+
+
 module.exports = router;
