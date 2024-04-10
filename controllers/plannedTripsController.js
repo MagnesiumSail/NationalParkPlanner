e96373cbcd065bb9accd41b1e949d0fb370f8779
sofiagalkina <gal18024@byui.edu>
2024-04-09 22:19:50 -0600

    get single planned trip runs and working

--- controllers/plannedTripsController.js
+++ controllers/plannedTripsController.js
@@ -22,4 +22,44 @@ async function retrieveAllPlannedTrips(req, res) {
     }
 }
 
-module.exports = { retrieveAllPlannedTrips};
+
+// GET one specific planned trip ID:
+async function retrievePlannedTripsById(req, res) {
+    try {
+        const db = getDb();
+        if (!db) {
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const plannedTripsId = req.params.id; // Retrieve Planned trips ID from URL params
+        if (!ObjectId.isValid(plannedTripsId)) {
+            return res.status(400).json({ error: 'Invalid planned trip ID' });
+        }
+
+        const plannedTripsCollection = db.collection('PlannedTrips');
+        const plannedTrips = await plannedTripsCollection.findOne({ _id: new ObjectId(plannedTripsId) });
+
+        if (!plannedTrips) {
+            return res.status(404).json({ error: 'Planned Trip not found' });
+        }
+
+        res.json(plannedTrips);
+
+        console.log("Function working");
+        console.log("Retrieved Planned Trip:", plannedTrips);
+    } catch (error) {
+        console.error('Error retrieving planned trip by ID:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = { retrieveAllPlannedTrips, retrievePlannedTripsById};
--- routes/plannedTripsRoute.js
+++ routes/plannedTripsRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const PlannedTripsController = require('../controllers/plannedTripsController');
 
-const router = express.Router()
+const router = express.Router();
+router.use(express.json());
+
 
 router.get('/', PlannedTripsController.retrieveAllPlannedTrips);
-
+// get a specific plannedtrip by id:
+router.get('/:id', PlannedTripsController.retrievePlannedTripsById);
 module.exports = router;
